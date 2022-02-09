08764bd51328a45ec41116f6c4669c50ca129360
Stanislas Bernard <bernardstanislas@gmail.com>
2022-02-09 16:37:52 +0100

    :sparkles: Bounties are displayable

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
+use std::fmt;
+
 #[derive(Debug)]
 struct Bounty {
     title: String,
     description: String,
     amount: u32,
     payment_token_address: String
+}
+
+
+impl fmt::Display for Bounty {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} [{} at {}]: {}", self.title, self.amount, self.payment_token_address, self.description)
+    }
 }
 
 #[cfg(test)]
@@ -23,5 +32,17 @@ mod tests {
         assert_eq!(bounty.description, "Develop a bounty platform");
         assert_eq!(bounty.amount, 100);
         assert_eq!(bounty.payment_token_address, "0x0000000000000000000000000000000000000000");
+    }
+
+    #[test]
+    fn test_bounty_display() {
+        let bounty = Bounty {
+            title: "Bounty platform".to_string(),
+            description: "Develop a bounty platform".to_string(),
+            amount: 100,
+            payment_token_address: "0x0000000000000000000000000000000000000000".to_string()
+        };
+
+        assert_eq!(format!("{}", bounty), "Bounty platform [100 at 0x0000000000000000000000000000000000000000]: Develop a bounty platform");
     }
 }
