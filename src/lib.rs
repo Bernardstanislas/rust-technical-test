9d6f2a6f0332d99a21947db87b7d3f3e1139f540
Stanislas Bernard <bernardstanislas@gmail.com>
2022-02-09 17:08:47 +0100

    :recycle: Move store logic to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::fmt;
-use std::fs::File;
-use std::io::{Write, Read, Error, Seek, SeekFrom};
+mod store;
+mod prompt;
 
 #[derive(Debug)]
-struct Bounty {
+pub struct Bounty {
     title: String,
     description: String,
     amount: u32,
@@ -13,20 +13,6 @@ struct Bounty {
 impl fmt::Display for Bounty {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} [{} at {}]: {}", self.title, self.amount, self.payment_token_address, self.description)
-    }
-}
-
-trait BountyStore {
-    fn save_bounty(&mut self, bounty: Bounty) -> Result<(), Error>;
-}
-
-struct FileBountyStore<'a> {
-    store_file: &'a File
-}
-
-impl BountyStore for FileBountyStore<'_> {
-    fn save_bounty(&mut self, bounty: Bounty) -> Result<(), Error> {
-        write!(self.store_file, "{}", bounty)
     }
 }
 
@@ -59,24 +45,5 @@ mod tests {
         };
 
         assert_eq!(format!("{}", bounty), "Bounty platform [100 at 0x0000000000000000000000000000000000000000]: Develop a bounty platform");
-    }
-
-    #[test]
-    fn test_bounty_file_store() {
-        let bounty = Bounty {
-            title: "Bounty platform".to_string(),
-            description: "Develop a bounty platform".to_string(),
-            amount: 100,
-            payment_token_address: "0x0000000000000000000000000000000000000000".to_string()
-        };
-        let mut tmpfile: File = tempfile::tempfile().unwrap();
-        let mut bounty_store = FileBountyStore { store_file: &tmpfile };
-
-        bounty_store.save_bounty(bounty).unwrap();
-        tmpfile.seek(SeekFrom::Start(0)).unwrap();
-
-        let mut buffer = String::new();
-        tmpfile.read_to_string(&mut buffer).unwrap();
-        assert_eq!("Bounty platform [100 at 0x0000000000000000000000000000000000000000]: Develop a bounty platform", buffer);
     }
 }
---
+++ src/store.rs
@@ -0,0 +1,42 @@
+use std::fs::File;
+use std::io::{Write, Error};
+use super::Bounty;
+
+pub trait BountyStore {
+    fn save_bounty(&mut self, bounty: Bounty) -> Result<(), Error>;
+}
+
+pub struct FileBountyStore<'a> {
+    store_file: &'a File
+}
+
+impl BountyStore for FileBountyStore<'_> {
+    fn save_bounty(&mut self, bounty: Bounty) -> Result<(), Error> {
+        write!(self.store_file, "{}", bounty)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::{Read, Seek, SeekFrom};
+
+    #[test]
+    fn test_bounty_file_store() {
+        let bounty = Bounty {
+            title: "Bounty platform".to_string(),
+            description: "Develop a bounty platform".to_string(),
+            amount: 100,
+            payment_token_address: "0x0000000000000000000000000000000000000000".to_string()
+        };
+        let mut tmpfile: File = tempfile::tempfile().unwrap();
+        let mut bounty_store = FileBountyStore { store_file: &tmpfile };
+
+        bounty_store.save_bounty(bounty).unwrap();
+        tmpfile.seek(SeekFrom::Start(0)).unwrap();
+
+        let mut buffer = String::new();
+        tmpfile.read_to_string(&mut buffer).unwrap();
+        assert_eq!("Bounty platform [100 at 0x0000000000000000000000000000000000000000]: Develop a bounty platform", buffer);
+    }
+}
